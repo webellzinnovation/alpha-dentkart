466942783727f14dad10c987a8f9cfef3e10eec6
webellzinnovations-4661 <webellzinnovations-4661@gmail.com>
2026-03-26 22:34:57 +0530

    fix: Update Node.js to 22, fix capacitor config for build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,4 +1,3 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
@@ -11,30 +10,25 @@ const config: CapacitorConfig = {
   ios: {
     scheme: 'alphadentkart'
   },
-  '@capacitor/splash-screen',
-  '@capacitor/status-bar',
-  '@capacitor/geolocation',
-  '@capacitor/network',
-  '@capacitor/camera',
-  '@capacitor/file-system',
-  '@capacitor/local-notifications',
-  '@capacitor/webview',
-  '@capacitor/haptics',
-  '@capacitor/share',
-  '@capacitor/push-notifications',
-  '@capacitor/keyboard',
-  '@capacitor/app-launcher'
-  ],
-android: {
-  buildOptions: {
-    webContentCompression: 'true'
+  android: {
+    backgroundColor: '#DD3B5F'
+  },
+  plugins: {
+    /* plugins configure here if needed, the core plugins are auto-registered */
+    // '@capacitor/splash-screen',
+    // '@capacitor/status-bar',
+    // '@capacitor/geolocation',
+    // '@capacitor/network',
+    // '@capacitor/camera',
+    // '@capacitor/file-system',
+    // '@capacitor/local-notifications',
+    // '@capacitor/webview',
+    // '@capacitor/haptics',
+    // '@capacitor/share',
+    // '@capacitor/push-notifications',
+    // '@capacitor/keyboard',
+    // '@capacitor/app-launcher'
   }
-},
-ios: {
-  buildOptions: {
-    webContentCompression: 'true'
-  }
-}
 };
 
 export default config;
