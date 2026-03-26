5adc16ee99c008a33802e9c3d4d1b5450b39026a
webellzinnovations-4661 <webellzinnovations-4661@gmail.com>
2026-03-26 23:28:30 +0530

    fix: Match package name com.adk.app for Android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.alphadentkart.app',
+  appId: 'com.adk.app',
   appName: 'Alpha Dentkart',
   webDir: 'dist',
   server: {
