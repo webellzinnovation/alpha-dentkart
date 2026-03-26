aeb71e965f2bccc8ab77409adf8eccce228dbf5f
webellzinnovations-4661 <webellzinnovations-4661@gmail.com>
2026-03-26 17:44:35 +0530

    Update for Android APK build

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => {
           target: 'http://localhost:3001',
           changeOrigin: true,
           secure: false,
+          timeout: 60000, // 60 second proxy timeout
         }
       }
     },
